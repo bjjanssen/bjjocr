994e128429bb17eff18ee013840a24b6bf4302b7
bjjanssen <bjoern.janssen@gmail.com>
2014-07-20 23:32:37 +0200

    Several adaptions to our pwds.

--- OCRFileRenamer.bash
+++ OCRFileRenamer.bash
@@ -1,62 +1,93 @@
 #!/bin/bash
 
 # This is a simple OCR-based file renamer.
-# Last modified 2014-07-20 bjoern.janssen
-
-### Globals ###
-
-UZN="1900 50 250 50 PerNR"
-NN="NoName"
-EXT=".pdf"
-
-# Later we want to run several renamers in parallel. The simplest way to do so with BASH is to start several processes. 
+# 1) Install required packages
 #
-# burstPDF $f dir_$t &
+# $apt-get install tesseract-ocr tesseract-ocr-<yourlanguage> imagemagick pdftk
 #
-# Beware, this can make your machine unresponsive if you have several PDFs with many pages (>1000). 
+# 2) Convert your source file to a tiff-image with imagemagick's convert tool. The tiff must fullfill the following requirement
+# * 300dpi
+# * 8 bit colordepth
+#
+# I recommend stripping and trimming, too.
+#
+# $convert -depth 8 -density 300 -strip -trim input.pdf output.tiff
+#
+# 3) Use zonal OCR with tesseract. 
+#
+# Create an uzn file to define a zone. 
+# The uzn file must have the same name as the input file, e.g input file = input.tiff then the uzn file must be named input.uzn
+# The uzn file format is:
+# x-coordinate y-coordinate width height identifier
+#
+# The x- and y-coordinates define the top left corner of a rectangle. 
+# Width and height define the dimensions of the rectangle. 
+# The identifier is unused and currently only helps the user in remembering the defined zone.
+# All parameters must one space apart.
+#
+# You can define several zones, but only the first one is used.
+#
+# $tesseract input.tiff - -l <yourlanguage> -psm 4 
+#
+# Instead of stdout you can use a filename.
+#
+############################################
 
-function createWorkingArea {
-	mkdir Vorlagen
+function createWorkingDirs {
 	for f in *.pdf
 	do 
-		pre=$(basename $f .pdf)
-		# PDFTK
-		pdftk $f burst output ${pre}_%05d.pdf
-		mv $f Vorlagen/
-		worker $pre
-
+		t=$(basename $f .pdf)
+		mkdir dir_$t
+		mv $f dir_$t
+		burstPDF $f dir_$t &
 	done
 }
 
-function worker {
-	pre=$1
-	for file in ${pre}*.pdf
+function burstPDF {
+	file=$1
+	dir=$2
+	cd $dir
+	pdftk $file burst
+	startWorker
+}
+
+function startWorker {
+	for f in *.pdf
 	do
-		base=$(basename $file .pdf)
-		echo $UZN > $base.uzn
-		convert -depth 8 -density 300 -trim -strip $file $base.tiff
-		ocr $base.tiff tmp_$base deu 4
-
-		persid=$(grep -P '[0-9]{4,8}' tmp_$base.txt)
-		echo $number
-		NN="LA_06_2014_$persid"
-
-		rm -f $base.tiff 
-		rm -f $base.uzn
-		rm -f tmp_$base.txt
-
-		count=1
-		FNN=$NN$EXT
-		
-		while [[ -e $FNN ]]
-		do
-			incr=$(printf "%03d" $count)
-			FNN="$NN.$incr"
-			let count=count+1
-		done
-
-		mv -n -v $file $FNN
+		renamePDF $f &
 	done
+}
+
+function checkDoubles {
+	name=$1
+	let count=0
+	incr=($printf "%05d" $count)
+
+	if [ -e $name ]
+	then 
+		n=$(basename $newname .pdf)
+		newname="$n $incr.pdf"
+		let count=count+1
+		checkDoubles $newname
+	else
+		break
+	fi
+}
+
+function renamePDF {
+	file=$1
+	base=$(basename $f .pdf)
+	convert -depth 8 -density 300 -trim -strip $file $base.tiff
+	ocr $base.tiff tmp_$base deu 4
+
+	PersID=$(grep -P '[0-9]{4,8}' tmp_$base.txt)
+	
+	nn="LA_06_2014_$PersID.pdf"
+
+	checkDoubles $nn
+
+	mv -n -v $file $nn
+	rm -f $base.tiff 
 }
 
 function ocr {
@@ -64,9 +95,10 @@ function ocr {
 	output=$2
 	lang=$3
 	level=$4
-	tesseract $input $output -l $lang -psm $level &> /dev/null
+	tesseract $input $output -l $lang -psm $level
 }
 
-createWorkingArea
+
+createWorkingDirs
 exit
 
